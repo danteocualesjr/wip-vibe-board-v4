06c525c34ae63113e3c545249ba8f74023419cce
Dante O. Cuales, Jr. <danteocualesjr@gmail.com>
2025-07-05 05:49:05 +0800

    UI tweaks

--- src/components/ui/accordion.tsx
+++ src/components/ui/accordion.tsx
@@ -22,6 +22,7 @@ const AccordionItem = React.forwardRef<
 AccordionItem.displayName = "AccordionItem"
 
 const AccordionTrigger = React.forwardRef<
+
   React.ElementRef<typeof AccordionPrimitive.Trigger>,
   React.ComponentPropsWithoutRef<typeof AccordionPrimitive.Trigger>
 >(({ className, children, ...props }, ref) => (
